1e3d3ec7cc745fa33a6d8588e1c59f247a073c2a
cicerohen <cicerohen@gmail.com>
2019-04-08 23:38:43 -0300

    Finish series

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -2,14 +2,18 @@ class Series
   def initialize(series = '')
     @series = series
   end
-  def slices(chunk = 1)
-    @series..each_with_index.map {
+  def slices(splitter = 0)
+    @series.split("").each_with_index.map {
       |item, index|
+      if (splitter > @series.length)
+        raise ArgumentError.new
+      end
+      spltr = splitter <= 0 && @series.length || splitter
       start_at = index
-      end_at = index+1
+      end_at = (index-1)+spltr
       if end_at < @series.length
         @series[start_at..end_at]
-      end 
+      end
     }.compact
   end
 end
